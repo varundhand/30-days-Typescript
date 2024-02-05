09825d49b88ce628ce20c5a28a21f2ff9e82e3ad
Varun Dhand <varundhand77@gmail.com>
2024-02-05 16:52:39 +0530

    lesson 1: explicit types, setting up ts

---
+++ Lesson 1/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = 'varun';
+console.log(username);
+let a = 1;
+let b = '2';
+let c = 3;
+console.log(a / b); // throws error in compile time unlike js as typescript is statically typed.
+console.log(a * b);
---
+++ Lesson 1/src/main.ts
@@ -0,0 +1,10 @@
+let username = 'varun'
+
+console.log(username)
+
+let a: number = 1 
+let b: string = '2' 
+let c: number = 3
+
+console.log(a/b); // throws error in compile time unlike js as typescript is statically typed.
+console.log(a*b);
